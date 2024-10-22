d8c4e06df3635ec32e988b36c021fadd41b356dd
EnanderW <williamenander@gmail.com>
2024-10-22 12:19:39 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿namespace lektion15;
+
+public class RequestThread {
+    public async static Task<APIQuote> Run() {
+        // En "stopwatch" som används för att räkna ut
+        // hur lång tid det tar att köra denna metod.
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+
+        // Denna instans behövs för att kunna skicka iväg en HTTP request
+        HttpClient httpClient = new HttpClient {
+            BaseAddress = new Uri("https://dummyjson.com/")
+        };
+
+        // Denna skapar själva meddelandet som skall skickas
+        HttpRequestMessage message = new HttpRequestMessage(
+            HttpMethod.Get,
+            "quotes/random"
+        );
+
+        // https://dummyjson.com/quotes/random
+
+        // Skicka iväg själva meddelandet och få tillbaka ett svar
+        HttpResponseMessage response = await httpClient.SendAsync(message);
+
+        // Man måste läsa av innehållet (body) av svaret genom en StreamReader
+        StreamReader reader = new StreamReader(response.Content.ReadAsStream());
+    
+        // Anropa koden som faktiskt hämtar ut innehållet i form av en sträng
+        string body = reader.ReadToEnd();
+
+        // Omvandla strängen (som är i JSON format) till ett objekt
+        APIQuote quote = System.Text.Json.JsonSerializer.Deserialize<APIQuote>(
+            body, 
+            new System.Text.Json.JsonSerializerOptions {
+                PropertyNameCaseInsensitive = true
+            }
+        )!;
+
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+        Console.WriteLine("Took " + elapsedMs + "ms");
+        
+        return quote;
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        
+        // Starta igång alla funktioner samtidigt
+        Task<APIQuote> task1 = RequestThread.Run();
+        Task<APIQuote> task2 = RequestThread.Run();
+        Task<APIQuote> task3 = RequestThread.Run();
+
+        // Vänta på att alla funktioner blir klara.
+        // Detta får effekten av att skicka 3 HTTP meddelanden samtidigt.
+        await Task.WhenAll(task1, task2, task3);
+
+        Console.WriteLine(task1.Result.Author);
+        Console.WriteLine(task2.Result.Author);
+        Console.WriteLine(task3.Result.Author);
+
+        var elapsedMs = watch.ElapsedMilliseconds;
+        Console.WriteLine("Everything took " + elapsedMs + "ms");
+    }
+}
+
+public class APIQuote {
+    public int Id { get; set; }
+    public string Quote { get; set; }
+    public string Author { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+---
+author: Lektion 15 
+date: MMMM dd, YYYY
+paging: "%d / %d"
+---
+
+# Lektion 15
+
+Hej och välkommen!
+
+## Agenda
+
+1. Frågor och repetition
+2. Introduktion till HTTP & HttpClient
+3. Introduktion till trådar
+4. Introduktion till async/await
+5. Grupparbete med handledning
+
+---
+
+# Fråga
+
+Kommer vi gå igenom vilken typ av kommentarer som är viktigt att få med i våran kod, och när det kanske är överflödigt att lägga in en kommentar? 
+
+Hur och när bör vi kommentera i koden och är det något som vi förväntas göra i våra inlämningar?
+
+# Svar
+
+Det finns inga krav på att kommentera koden, och det förväntas heller inte. Vi kan prata mer om kommentarer i framtiden, men kort nu:
+
+Kommentera kod som är svår att förstå genom att bara läsa koden.
+
